8c5dbd7ec346c5273629b6592ef64cfdfcb4ac95
ldrahnik <l.drahnik@gmail.com>
2015-01-06 13:54:09 +0100

    Added URL && PASSWORD regexps

--- src/regexp/Validator.php
+++ src/regexp/Validator.php
@@ -11,5 +11,7 @@ class Validator {
 	public static $messages = array(
 		Regexp::USERNAME => 'Please enter a username with these characters: (a-z), number (0-9), _, - and count between 3 and 16.',
 		Regexp::EMAIL => 'Please enter a valid email address.',
+		Regexp::PASSWORD => 'Please enter a password with there characters: (a-z), number (0-9), _, - and count between 6 and 18.',
+		Regexp::URL => 'Please enter a valid website address.',
 	);
 }
