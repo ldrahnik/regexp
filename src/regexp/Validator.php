cd6b59a33d4f804ed3bcaa67644cc45319f13b72
ldrahnik <l.drahnik@gmail.com>
2015-01-06 17:12:46 +0100

    Fixed validator messages as array

--- src/regexp/Validator.php
+++ src/regexp/Validator.php
@@ -11,8 +11,8 @@ class Validator {
 
 	/** @var array */
 	public static $messages = array(
-		Regexp::USERNAME => 'Please enter a username with these characters: (a-z), number (0-9), _, - and count between 3 and 16.',
-		Regexp::PASSWORD => 'Please enter a password with there characters: (a-z), number (0-9), _, - and count between 6 and 18.',
-		Regexp::URL => 'Please enter a valid website address.',
+		'USERNAME' => 'Please enter a username with these characters: (a-z), number (0-9), _, - and count between 3 and 16.',
+		'PASSWORD' => 'Please enter a password with there characters: (a-z), number (0-9), _, - and count between 6 and 18.',
+		'URL' => 'Please enter a valid website address.',
 	);
 }
