9de7404564a724f1331e311a6469c1835270f77c
Lukáš Drahník <L.Drahnik@gmail.com>
2015-01-16 02:25:10 +0100

    Removed also URL validator

--- src/regexp/RegExp.php
+++ src/regexp/RegExp.php
@@ -14,7 +14,6 @@ class RegExp {
 		USERNAME = '^[a-z0-9_-]{3,16}$',
 		TWITTER_USERNAME = '^(\@)?[A-Za-z0-9_]+$',
 		PASSWORD = '^[a-z0-9_-]{6,18}$',
-		URL = '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$',
 		FACEBOOK = '^(https?:\/\/)?(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]',
 		GOOGLE = '((http|https):\/\/)?(www[.])?plus\.google\.com\/.?\/?.?\/?([0-9]*)';
 }
