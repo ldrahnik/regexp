aea910c5e927dafc1f49752eb49e90875eb3f5e0
Lukáš Drahník <L.Drahnik@gmail.com>
2015-01-20 23:59:15 +0100

    Added autoload scr folder its necessary

--- tests/regexp/bootstrap.php
+++ tests/regexp/bootstrap.php
@@ -23,7 +23,7 @@ $configurator = new \Nette\Configurator;
 $configurator->setDebugMode(FALSE);
 $configurator->setTempDirectory(__DIR__ . '/temp');
 $configurator->createRobotLoader()
-	//->addDirectory(__DIR__ . '/../../src')
+	->addDirectory(__DIR__ . '/../../src')
 	->register();
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 
